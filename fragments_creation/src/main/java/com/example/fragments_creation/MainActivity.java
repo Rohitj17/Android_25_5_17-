453c2d6d01d58103eb0192e7fa9a2d4b3d1f4916
Rohitj17 <Rohitjere17@gmail.com>
2017-06-05 12:21:05 +0530

    Fragments single fragment used multiple times reusabilty BackStack added

--- fragments_creation/src/main/java/com/example/fragments_creation/MainActivity.java
+++ fragments_creation/src/main/java/com/example/fragments_creation/MainActivity.java
@@ -15,10 +15,13 @@ public class MainActivity extends AppCompatActivity {
         loadFragment(First_Fragment.getInstance(R.mipmap.ic_launcher_round));
     }
     public void loadFragment(Fragment frag) {
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction txn = manager.beginTransaction();
+        String back=frag.getClass().getName();
+        FragmentManager manager = getSupportFragmentManager();//for interacting with fragments and it
+         //returns FragmentManger  obj
+        FragmentTransaction txn = manager.beginTransaction();//starts series of edit operation on fragments related to  FragmentManger
         txn.setCustomAnimations(R.anim.enter, R.anim.exit);
-        txn.replace(R.id.frag1, frag);
-        txn.commit();
+        txn.replace(R.id.frag1, frag);//replaces current frag
+        txn.addToBackStack(back);
+        txn.commit();//commits transaction & executes all commands
     }
 }
