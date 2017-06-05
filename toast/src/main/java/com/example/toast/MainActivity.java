4c0307ea13fd6c461da1710869d67851853db80d
Rohitj17 <Rohitjere17@gmail.com>
2017-06-05 12:49:07 +0530

    Toast basic

---
+++ toast/src/main/java/com/example/toast/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.toast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onToast(View view)
+    {
+        Toast t= Toast.makeText(this,"Hey Buddy!",Toast.LENGTH_LONG);
+        Button b=new Button(this);
+        b.setText("Learning Android!!");
+        t.setView(b);
+    }
+}
